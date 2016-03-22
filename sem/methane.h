0814bc345988cfcd05b399299660e38cde701575
Nicholas Hudeck <nxh177@case.edu>
2016-03-22 15:46:10 -0400

    update methan.h
    Signed-off-by: Nicholas Hudeck <nxh177@case.edu>

--- sem/methane.h
+++ sem/methane.h
@@ -11,7 +11,50 @@
 #include <time.h>
 #include <unistd.h>
 
+#define SHMKEY 717
+#define SEMKEY 717
+
 #define NUM_HYDROGEN 4
 #define NUM_CARBON 1
 
 
+union semun {
+  unsigned short *array;
+}
+
+/*
+  define a struct that will hold
+  all shared variables
+*/
+struct common {
+  int waiting_C = 0;
+  int waiting_H = 0;
+}
+
+void semWait(int semid, int semaphore){
+  struct sembuf psembuf;
+
+  psembuf.sem_op = -1;
+  psembuf.sem_flg = 0;
+  psembuf.sem_num = semaphore;
+
+  /*
+    executes the operation that is being
+    built just above
+  */
+  semop(semit, &psembuf, 1);
+}
+
+void semSignal(int semid, int semaphore){
+  struct sembuf vsembuf;
+
+  vsembuf.sem_op = 1;
+  vsembuf.sem_flg = 0;
+  vsembuf.sem_num = semaphore;
+
+  /*
+    executes the operation that is being
+    built just above
+  */
+  semop(semit, &psembuf, 1);
+}

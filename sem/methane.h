9cf0e81635bdf53ca383bc078ac423c6ea490a19
Nicholas Hudeck <nxh177@case.edu>
2016-03-22 15:22:11 -0400

    changed methane.h
    Signed-off-by: Nicholas Hudeck <nxh177@case.edu>

---
+++ sem/methane.h
@@ -0,0 +1,17 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/smh.h>
+#include <sys/errno.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define NUM_HYDROGEN 4
+#define NUM_CARBON 1
+
+

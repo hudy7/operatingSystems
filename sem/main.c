07a91c514fa6c836449f586cb039231201045239
Nicholas Hudeck <nxh177@case.edu>
2016-03-23 21:17:43 -0400

    added some content to hydrogen method
    Signed-off-by: Nicholas Hudeck <nxh177@case.edu>

--- sem/main.c
+++ sem/main.c
@@ -188,6 +188,29 @@ void hydrogen(void){
   //printVariables();
 
   //printf("Printing in hydrogen: waiting_H: %d,  waiting_C: %d", shared->waiting_H, shared->waiting_C);
+  int semid, shmid;//semaphore memory id, shared memory id
+
+  int pid = getpid();
+
+  //get semaphore memory id
+	if ((semid = semget(SEMKEY, NUM_SEMS, 0777)) < 0) {
+		perror("semget");
+		exit(EXIT_FAILURE);
+	}
+
+	//get shared memory id
+  if ((shmid = shmget(SHMKEY, 1*K, 0777)) < 0) {
+  	perror("shmget");
+  	exit(EXIT_FAILURE);
+  }
+
+  //get pointer to shared data structure
+  if ((shared = (struct common *)shmat(shmid, 0, 0)) < 0) {
+  	perror("shmat");
+  	exit(EXIT_FAILURE);
+  }
+
+  printf("waiting_H: %d,  waiting_C: %d", shared->waiting_H, shared->waiting_C);
 
 
 

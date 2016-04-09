ef8586a8b47e031a49f4fdecf04dd1222d708d85
Nicholas Hudeck <nxh177@case.edu>
2016-04-09 12:32:02 -0400

    initial files
    Signed-off-by: Nicholas Hudeck <nxh177@case.edu>

---
+++ pthreads/carbon.c
@@ -0,0 +1,45 @@
+/*
+Nicholas Hudeck
+EECS 338 - Assignment 5
+A Semaphore-based Solution to Sequential CH4 Production Problem
+
+carbon.c
+*/
+#include "methane.h"
+
+struct common *shared;
+
+int main(int argc, char *argv[]){
+  int semid = atoi(argv[1]);
+  int shmid = atoi(argv[2]);
+	int pid = getpid();
+
+	if ((shared = (struct common *)shmat(shmid, 0, 0)) < 0) {
+		perror("CARBON: shmat");
+		exit(EXIT_FAILURE);
+	}
+
+  printAtom(true);
+	semWait(semid, MUTEX);
+
+	fflush(stdout);
+	printf("Carbon atom %d created\n", pid);
+	fflush(stdout);
+
+	printBarrier(true, shared->waiting_C, shared->waiting_H);
+
+	if (shared->waiting_H >= 4) {
+		for(int i = 0; i < 4; i++){
+      	semSignal(semid, SH);
+    }
+		shared->waiting_H -= 4;
+		semSignal(semid, MUTEX);
+		printCH4();
+	} else {
+		shared->waiting_C += 1;
+		semSignal(semid, MUTEX);
+		semWait(semid, SC);
+	}
+
+	return EXIT_SUCCESS;
+}

3bbd1cd0dc4d30e1fa9f4018c6f9663f730bee46
Nicholas Hudeck <nxh177@case.edu>
2016-04-28 15:21:04 -0400

    msg
    Signed-off-by: Nicholas Hudeck <nxh177@case.edu>

--- as7/as7_server.c
+++ as7/as7_server.c
@@ -1,78 +1,56 @@
-/**
- * The function templates in this code were developed by rpcgen.
- * The functionality was programmed in for as7, eecs 338.
- * @author Shaun Howard
- */
+/*as7_server.c Nick Hudeck*/
 
 #include "as7.h"
 #include <time.h>
 
-// set message limit for server
-#define MSG_LIMIT 15
 
-// track whether the client data has been initialized
-int is_initialized = 0;
 
-// store the current time in an array of 26 chars
-char curr_time[26];
+char currentTime[26]; // store the current time in this string/char array
+struct data clientMessages[15]; // allocating space for messages
 
-// allocate space for each of 3 clients to put 5 messages on the server
-struct data client_msgs[MSG_LIMIT];
+/* initialize message IDs to -1 and adjust them as they come in */
+int messageIDs[15] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+int currentMessage = 0; // current client message index
+int currentClient = 0; //maintain when we get a new client
+int currentClientIndex[3] = {-1, -1, -1}; //keep track of three clients each having messages
+int clientList[3];
 
-// initialize all client msg ids to -1, incrementally change based on messages put in fcfs order
-int client_msg_ids[15] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-
-// store the current client msg index, which should be at max equal to MSG_LIMIT
-int curr_index = 0;
-
-// store the current client index to maintain a new client addition to client list
-int curr_client = 0;
-
-// track msg indices for each client
-int curr_client_indices[3] = {-1, -1, -1};
-
-// store the current client ids in a list, only support 3 clients
-int client_list[3];
-
-// sets the current time and date to the "curr_time" char buffer
-void set_time() {
+/* get the current time, mostly found on stack overflow */
+void getTime() {
 	time_t rawtime;
 	struct tm * timeinfo;
-
-	// reinitialize curr_time buff if it is null
-	if (curr_time == NULL) {
-		curr_time[0] = 0;
+	if (currentTime == NULL) {
+		currentTime[0] = 0;
 	}
-
-	// get the current time in ms
+	//current time in milliseconds
 	time (&rawtime);
 	if (rawtime == ((time_t)-1)) {
-		perror("error generating time information...");
-		curr_time[0] = 0;
+		perror("rawtime error");
+		currentTime[0] = 0;
 		return;
 	}
 
-	// convert time to local time
+	// gets in local time
 	timeinfo = localtime (&rawtime);
     if (timeinfo == NULL) {
-        perror("error converting time to local time...");
-        curr_time[0] = 0;
+        perror("local time error");
+        currentTime[0] = 0;
         return;
     }
 
-    // convert time to asctime
-    asctime_r(timeinfo, curr_time);
-	if (curr_time == NULL) {
-		perror("error converting time to asc time...");
-		curr_time[0] = 0;
+    // gets time a readable manner
+    asctime_r(timeinfo, currentTime);
+	if (currentTime == NULL) {
+		perror("readable time error");
+		currentTime[0] = 0;
 		return;
 	}
-
+/*
 	// remove newline from end, just put terminator
-	int new_line = strlen(curr_time) - 1;
-	if (curr_time[new_line] == '\n') {
-		curr_time[new_line] = '\0';
-	}
+	int new_line = strlen(currentTime) - 1;
+	if (currentTime[new_line] == '\n') {
+		currentTime[new_line] = '\0';
+	}*/
 }
 
 int *
@@ -87,8 +65,8 @@ get_1_svc(int *argp, struct svc_req *rqstp)
 	}
     *result = -1;
 	// set the most updated time for the global time array
-    set_time();
-	printf("[%s] Server received a GET request from client %d.\n", curr_time, *argp);
+    getTime();
+	printf("[%s] Server received a GET request from client %d.\n", currentTime, *argp);
     fflush(stdout);
 
 	//check if there are any messages received from clients other than the one specified
@@ -100,7 +78,7 @@ get_1_svc(int *argp, struct svc_req *rqstp)
     // get client id
     for (i = 0; i < 3; i++) {
     	// check to see where the client is in the server client list
-    	if (client_list[i] == real_client_id){
+    	if (clientList[i] == real_client_id){
     		// found the client, now set it to keep track
     		client_id = i;
     		// translated client id, now leave loop
@@ -109,15 +87,15 @@ get_1_svc(int *argp, struct svc_req *rqstp)
     }
 
     // see if the client has any messages to "get"
-    for (i = 0; i < MSG_LIMIT; i++){
+    for (i = 0; i < 15; i++){
     	// limit to only 3 clients
-    	if (client_id >= 0 && client_id <= 2 && i > curr_client_indices[client_id]){
+    	if (client_id >= 0 && client_id <= 2 && i > currentClientIndex[client_id]){
             // check if the client id is in the list of ids and not equal to this id
-			if (client_msg_ids[i] != real_client_id && client_msg_ids[i] >= 0){
+			if (messageIDs[i] != real_client_id && messageIDs[i] >= 0){
 				// set success status
 				*result = 0;
 				// update the current client message index
-				curr_client_indices[client_id] = i;
+				currentClientIndex[client_id] = i;
 				// found a message, now return
 				return result;
 			}
@@ -138,19 +116,19 @@ put_1_svc(struct data *argp, struct svc_req *rqstp)
 	}
 	*result = -1;
 	// set the most updated time for the global time array
-	set_time();
-	printf("[%s] Server received a PUT request from client %d.\n", curr_time, argp->id);
+	getTime();
+	printf("[%s] Server received a PUT request from client %d.\n", currentTime, argp->id);
     fflush(stdout);
 
     // only put new message if the message limit has not been met
-	if (curr_index < MSG_LIMIT){
+	if (currentMessage < 15){
 	    // should be between 0 and 2, else client not in list
 	    int client_id = -1;
 	    int i;
 	    // get client id
 	    for (i = 0; i < 3; i++) {
 	    	// check to see where the client is in the server client list
-	    	if (client_list[i] == argp->id){
+	    	if (clientList[i] == argp->id){
 	    		// found the client, now set it to keep track
 	    		client_id = i;
 	    		// translated client id, now leave loop
@@ -159,24 +137,24 @@ put_1_svc(struct data *argp, struct svc_req *rqstp)
 	    }
 
 		// store client msg
-	    if (strcpy(client_msgs[curr_index].message, argp->message) == NULL){
+	    if (strcpy(clientMessages[currentMessage].message, argp->message) == NULL){
 	    	perror("failure putting message on server...");
 	    } else {
 		    // store client id
-			client_msgs[curr_index].id = argp->id;
+			clientMessages[currentMessage].id = argp->id;
 
 			// store id in ordered id list
-			client_msg_ids[curr_index] = argp->id;
+			messageIDs[currentMessage] = argp->id;
 
 			// add user to client list only if necessary
 			if (client_id == -1){
 				// add this client to the clients list
-				client_list[curr_client] = argp->id;
+				clientList[currentClient] = argp->id;
 				// increment curr client counter
-				curr_client += 1;
+				currentClient += 1;
 			}
 			// increment current client msg index
-			curr_index += 1;
+			currentMessage += 1;
 			// set success result
 			*result = 0;
 	    }
